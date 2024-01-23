838ec3df91948a8db55797c4d947eba00c5b4ff0
Oleg Chukhlantsev <Nerevar5@mail.ru>
2024-01-23 09:58:23 +0300

    add postgres in docker compose.yaml

---
+++ src/main/java/com/example/dockerrestapiexample/DockerRestApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerrestapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRestApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerRestApiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerrestapiexample/DockerRestApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerrestapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerRestApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
